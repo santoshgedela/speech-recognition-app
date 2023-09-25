22050e503dc47522dd97493cb35b65f8ab051573
santoshge <santoshge.in@mouritech.com>
2023-09-25 12:22:47 +0530

    removed unecessary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SpeechRecognitionComponent } from './speech-recognition/speech-recognition.component';
-import { SpeechToTextComponent } from './speech-to-text/speech-to-text.component';
 
 const routes: Routes = [
   {
     path:"speechRecognition",
     component:SpeechRecognitionComponent
-  },
-  {
-    path:"speech",
-    component:SpeechToTextComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpeechRecognitionComponent } from './speech-recognition/speech-recognition.component';
-import { SpeechToTextComponent } from './speech-to-text/speech-to-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SpeechRecognitionComponent,
-    SpeechToTextComponent
   ],
   imports: [
     BrowserModule,
